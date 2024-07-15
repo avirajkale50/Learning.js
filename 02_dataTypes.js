d5a8d6931146fa857e302639a5c910ff3a06dc53
Aviraj Kale <125662965+avirajkale50@users.noreply.github.com>
2024-07-15 18:16:38 +0530

    datatype notes upload

--- 02_dataTypes.js
+++ 02_dataTypes.js
@@ -18,3 +18,26 @@ console.log("Hello");
 console.log("Sweater");
 
 // the different data types
+
+let name = "aviraj" 
+let age = 20 
+let isLoggedIn = false
+let state;
+let more = null;
+
+//number => 2 to the power 53
+//bigint => very big number
+//string => " "
+//boolean => true.false
+//null => standalone value 
+//undefined => nothing is assign
+// symbol => unquie , mostly use in react and figma
+
+
+//object
+console.log(typeof age);
+console.log(typeof more);
+console.log(typeof state);
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
