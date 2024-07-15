ceb87138b8c3de9f8a0ebee5eb359c3cfeccbd49
Aviraj Kale <125662965+avirajkale50@users.noreply.github.com>
2024-07-15 14:04:04 +0530

    notes update

--- 01_basics.js
+++ 01_basics.js
@@ -1,1 +1,8 @@
+//This is the first program 
+/*
+-The node is nothing but the software like compiler which is seprated form the browser to run javascript      individully 
+-There are other software to like deno 
+-Both founded by Ryan Dahl
+-Current version while learning is ECMAScript 2023
+*/
 console.log("Hello world");
