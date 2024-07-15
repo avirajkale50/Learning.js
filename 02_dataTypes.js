8f24e4fd4a76b799a28d4fcf4aac53956b8a6b1b
Aviraj Kale <125662965+avirajkale50@users.noreply.github.com>
2024-07-15 18:07:29 +0530

    minor update

---
+++ 02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict "; // treat all the js code as the newer version of the javascript
+
+//alert use different syntax in node ,this can perfectly work in the browser
+// alert("Hello world")
+
+//some bad practice while writing java script 
+/*
+console.log(3
+    +
+    3
+    )
+
+console.log("Hello");console.log("Sweater");
+*/
+
+//in good practice the code should be readable. for example,
+console.log("Hello");
+console.log("Sweater");
+
+// the different data types
