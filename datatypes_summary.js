c3c0ef273e7607d40a7c52a05d65b9dd139a132d
Avi Kale <avirajkale2004@gmail.com>
2024-08-03 20:30:59 +0530

    The stack and heap notes upload

--- datatypes_summary.js
+++ datatypes_summary.js
@@ -15,13 +15,13 @@ let largeValue = 546545466438761238794324n;
 
 
 //Now check the datatypes
-console.log(typeof score)//number
-console.log(typeof name)//string
-console.log(typeof temp)//object
-console.log(typeof work)//undefined
-console.log(typeof id)//symbol
-console.log(typeof isLoggedIn)//boolean
-console.log(typeof largeValue)//bigint
+// console.log(typeof score)//number
+// console.log(typeof name)//string
+// console.log(typeof temp)//object
+// console.log(typeof work)//undefined
+// console.log(typeof id)//symbol
+// console.log(typeof isLoggedIn)//boolean
+// console.log(typeof largeValue)//bigint
 
 //Note : The typeof null is object
 
@@ -29,7 +29,7 @@ console.log(typeof largeValue)//bigint
 let newId = Symbol('123')
 let anotherId = Symbol('123')
 
-console.log(newId === anotherId)
+// console.log(newId === anotherId)
 
 
 //Refernce (Non Primitive ) data types
@@ -50,9 +50,39 @@ const myFunction = function(){
 };
 
 //now testing the type with the typeof 
-console.log(typeof heros);//object
-console.log(typeof myObj);//object
-console.log(typeof myFunction);//function (function-object)
+// console.log(typeof heros);//object
+// console.log(typeof myObj);//object
+// console.log(typeof myFunction);//function (function-object)
 
 
 //Here we learn the basics of data types which are imp in the interview and also the typeof and some simple exceptions in JS
+
+
+//********************************************************Stack and Heap***************************************************
+
+// The memory in JavaScript allocated in the Stack or Heap
+
+//The primitive data type is stored in the stack and if we copy them into other variable there copy get copied not the actual data type 
+
+//Example 
+// let firstVariable = "nice";
+// let secondVariable = firstVariable
+// console.log(firstVariable,secondVariable);
+// secondVariable = "not nice"
+// console.log(firstVariable,secondVariable);
+
+//The non primitive datatypes is stored in the heap there name is in stack and the they refer the location at heap , and if we copy them the same location got refer so if we change the second variable the first variable also get change
+
+//Example 
+
+let oneObj ={
+    speed : 200,
+    bike : "discover"
+}
+let twoObj = oneObj
+console.log(oneObj,twoObj);
+
+twoObj.bike = "splender"
+console.log(oneObj,twoObj);
+
+
