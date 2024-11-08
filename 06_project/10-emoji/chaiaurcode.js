39fbadbd619988a5c985c0e3eaf58bf354451f92
avirajkale50 <avirajkale2004@gmail.com>
2024-11-08 21:52:37 +0530

    the 9-mouseCircle and 10-emoji is done

--- 06_project/10-emoji/chaiaurcode.js
+++ 06_project/10-emoji/chaiaurcode.js
@@ -1,4 +1,5 @@
 const btn = document.querySelector('#emoji');
+const text = document.querySelector('#text');
 const emojis = [
   '😆',
   '😅',
@@ -35,3 +36,18 @@ const emojis = [
   '😁',
   '😬',
 ];
+
+
+//writing a fuction so where user hover over emoji it will change
+
+btn.addEventListener("mouseover",(event)=>{
+  // console.log(btn);
+  let randomIndex = Math.floor(Math.random() * emojis.length);
+  btn.innerHTML = emojis[randomIndex];
+  let one = Math.floor(Math.random() * 255);
+  let two = Math.floor(Math.random() * 255);
+  let three = Math.floor(Math.random() * 255);
+  text.style.color = `rgba(${one}, ${two}, ${three}, 1)`;
+});
+
+//this is good
